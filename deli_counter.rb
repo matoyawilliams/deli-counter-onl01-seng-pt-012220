b129fe5efeee6893bd14a17c9ca390fbf5f2804b
Matoya Williams <matoyawilliams@yahoo.com>
2020-01-26 01:44:20 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,5 @@
-# Write your code here.
+katz_deli = ["The line is currently empty"]
+
+take_a_number(katz_deli#{name})
+count = 0 +=
+name= ["Ada","Grace","Kent","Matz"].join("Welcome#{name} you are number#{count}")
